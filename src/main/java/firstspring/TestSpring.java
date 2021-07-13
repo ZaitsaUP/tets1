38c47f990e5d9a089248ea6cda1af31c5ccbe569
Pavel Zaitsau <zaitsaup@gmail.com>
2021-07-14 00:48:28 +0300

    FIRST SPRING application context XML

--- src/main/java/firstspring/TestSpring.java
+++ src/main/java/firstspring/TestSpring.java
@@ -6,13 +6,6 @@ import java.util.MissingFormatArgumentException;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-          "applicationContext.xml"
-        );
 
-
-        Music rockMusic = context.getBean("rockMusicBean", RockMusic.class);
-        System.out.println(rockMusic.getSong());
-        context.close();
     }
 }
